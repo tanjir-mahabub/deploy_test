998d37195b55b46982d2804ea09bc2d48e12979b
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2021-12-27 00:48:28 +0600

    update index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Heading>Bismillahir Rahmanir Rahim 🎉</Heading>
   </Page>
 );
 
